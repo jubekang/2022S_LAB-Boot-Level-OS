9a82d5eda29c32927fd3112c9d28ccb9f02edfb7
강준범 <gangjunbeom@gangjunbeom-ui-MacBookPro.local>
2022-07-04 14:07:52 +0900

    Saving_registers

--- kernel.asm
+++ kernel.asm
@@ -32,10 +32,43 @@ End:
     jmp End
 
 handler0:
+    ; saving all register
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
     mov byte[0xb8000],'D'
     mov byte[0xb8001],0xd
 
     jmp End
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rbp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
 
     iretq
     
